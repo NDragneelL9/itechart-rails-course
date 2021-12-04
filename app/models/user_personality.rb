64fbdd68d53f2ccd4c403cb162abed8239216e0c
NDragneelL9 <nell.dragneel@gmail.com>
2021-12-04 18:08:58 +0300

    fix name uniqueness validation

--- app/models/user_personality.rb
+++ app/models/user_personality.rb
@@ -3,5 +3,7 @@ class UserPersonality < ApplicationRecord
 
   validates :name, presence: true,
                    length: { minimum: 3, maximum: 14 },
-                   uniqueness: { case_sensitive: false }
+                   uniqueness: { case_sensitive: false, scope: :user_id, 
+                                 message: "for personality already exists for this user" }
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_03_132431) do
+ActiveRecord::Schema.define(version: 2021_12_04_150201) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 2021_12_03_132431) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["name"], name: "index_user_personalities_on_name", unique: true
+    t.index ["user_id", "name"], name: "index_user_personalities_on_user_id_and_name", unique: true
     t.index ["user_id"], name: "index_user_personalities_on_user_id"
   end
 
