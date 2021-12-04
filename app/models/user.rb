8945c62dfe7402ed4a0eb4829d87add569c0fd5d
NDragneelL9 <nell.dragneel@gmail.com>
2021-12-04 18:07:49 +0300

    Create user personality when register a user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,16 @@
 class User < ApplicationRecord
   has_many :user_personalities, dependent: :destroy
+  after_create :create_default_personality
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  private 
+
+  def create_default_personality
+    @personality = UserPersonality.new(name: "Myself", user_id: self.id)
+    @personality.save
+  end
 end
