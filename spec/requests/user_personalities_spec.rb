8056e9435a66c5a018524045615605c2ab34840a
NDragneelL9 <nell.dragneel@gmail.com>
2021-12-06 21:38:17 +0300

    Added request tests to user_personalities

--- spec/requests/user_personalities_spec.rb
+++ spec/requests/user_personalities_spec.rb
@@ -3,42 +3,72 @@ require 'rails_helper'
 RSpec.describe 'UserPersonalities', type: :request do
   # rubocop:enable Metrics/BlockLength
   before do
-    user = User.create(email: 'test@test.com', password: 'password')
-    sign_in(user)
+    User.create(email: 'test@test.com', password: 'password')
+    user = User.first
+    sign_in user
   end
 
   describe 'GET routes tests' do
     it 'should show view to create personality' do
       get new_user_personality_path
       expect(response.body).to include 'Create personality'
+      expect(response).to have_http_status(200)
     end
 
     it 'should show index template' do
       get user_personalities_path
       expect(response.body).to include 'Choose your personality'
+      expect(response).to have_http_status(200)
     end
 
     it 'should show view show template' do
-      # FIXME: redirects to user_personalities_path instead of render :show template
-      personality = UserPersonality.create!(name: 'GrandPa',
-                                            user: User.create(
-                                              email: 'test@test.com', password: 'password'
-                                            ))
-      get user_personality_path(personality.id)
+      user = User.first
+      personality = UserPersonality.create(name: 'GrandPa', user: user)
 
-      # follow_redirect!
+      get user_personality_path(personality)
+      expect(response.body).to include 'GrandPa'
       expect(response).to have_http_status(200)
-      expect(response.body).to include 'Edit'
+    end
+
+    it 'should show view edit template' do
+      user = User.first
+      personality = UserPersonality.create(name: 'GrandPa', user: user)
+
+      get edit_user_personality_path(personality)
+      expect(response.body).to include 'Update personality'
+      expect(response).to have_http_status(200)
     end
   end
 
-  describe 'Post method' do
+  describe 'POST/PUT/PATCH routes tests' do
     it 'create userpersonality' do
-      get new_user_personality_path
-      expect(response).to have_http_status(200)
-
       post user_personalities_path, params: { user_personality: { name: 'GrandPa' } }
+
       expect(response).to have_http_status(302)
+    end
+
+    it 'update userpersonality' do
+      user = User.first
+      personality = UserPersonality.create(name: 'GrandPa', user: user)
+      newName = 'NewPerson'
+      patch user_personality_path(personality), params: { user_personality: { name: newName } }
+      expect(response).to have_http_status(302)
+      follow_redirect!
+
+      expect(response.body).to include newName
+      expect(response).to have_http_status(200)
+    end
+
+    it 'delete userpersonality' do
+      user = User.first
+      personality1 = UserPersonality.create(name: 'GrandPa', user: user)
+      personality2 = UserPersonality.create(name: 'NewPerson', user: user)
+      delete user_personality_path(personality1)
+      expect(response).to have_http_status(302)
+      follow_redirect!
+
+      expect(response.body).to_not include 'GrandPa'
+      expect(response).to have_http_status(200)
     end
   end
 end
