e4c531501b5deb90ee862c2e55c679ae86e717d9
NDragneelL9 <nell.dragneel@gmail.com>
2021-12-11 22:20:11 +0300

    add todo comment

--- app/models/user_personality.rb
+++ app/models/user_personality.rb
@@ -1,11 +1,10 @@
 class UserPersonality < ApplicationRecord
   belongs_to :user
   has_many :categories, dependent: :restrict_with_exception
-  # TODO: after creating transactions check 
+  # TODO: after creating transactions check
   #       make sure can delete personality with categories but without transactions
   validates :name, presence: true,
                    length: { minimum: 3, maximum: 14 },
                    uniqueness: { case_sensitive: false, scope: :user_id,
                                  message: 'for personality already exists for this user' }
-
 end
