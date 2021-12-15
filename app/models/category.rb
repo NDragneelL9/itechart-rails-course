9b38fad88d98f64757d74675369a71da4ac6b800
NDragneelL9 <nell.dragneel@gmail.com>
2021-12-15 22:23:34 +0300

    Created transaction model with refernce to category model In category model create one to many association with transactions In user_personality model update association (allow to delete personality with empty categories)

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ApplicationRecord
   belongs_to :user_personality
-
+  has_many :transactions, dependent: :restrict_with_exception
   validates :name, presence: true,
                    length: { minimum: 3, maximum: 14 },
                    uniqueness: { case_sensitive: false, scope: :user_personality_id,
---
+++ app/models/transaction.rb
@@ -0,0 +1,26 @@
+class Transaction < ApplicationRecord
+  belongs_to :category
+
+  before_update :rollback_update_action
+  after_save :update_category_amount
+
+  private
+
+  def update_category_amount
+    @amount_after_transaction = if withdrawal
+                                  category.amount_cents - amount_cents
+                                else
+                                  category.amount_cents + amount_cents
+                                end
+    category.update(amount_cents: @amount_after_transaction)
+  end
+
+  def rollback_update_action
+    @amount_after_rollback = if withdrawal_was
+                               category.amount_cents + amount_cents_was
+                             else
+                               category.amount_cents - amount_cents_was
+                             end
+    category.update(amount_cents: @amount_after_rollback)
+  end
+end
