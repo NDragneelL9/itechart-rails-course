359eb364ba180427799a2e08477e58ca4cc35c0c
NDragneelL9 <nell.dragneel@gmail.com>
2021-12-15 22:26:53 +0300

    added transactions routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :user_personalities do
-    resources :categories
+    resources :categories do 
+      resources :transactions, shallow: true
+    end
   end
   devise_for :users
   root 'pages#home'
