21aa3fb0856bf4a399a1a71097e3ef26dac5907a
NDragneelL9 <nell.dragneel@gmail.com>
2022-01-07 13:02:01 +0300

    created graphics page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,19 @@
 # Controller to pages entities
 class PagesController < ApplicationController
   def index; end
+
+  def graphics
+    time_now = Time.zone.now
+    @date_from = time_now.last_month.to_date
+    @date_to = time_now.to_date.next_day
+    transactions_date_range if params[:search].present?
+    @personality = UserPersonality.find(params[:user_personality_id])
+  end
+
+  private
+
+  def transactions_date_range
+    @date_from = Date.parse(params[:search][:date_from])
+    @date_to = Date.parse(params[:search][:date_to])
+  end
 end
