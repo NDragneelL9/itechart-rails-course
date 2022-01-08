27101477c8c912662d5c11653fd7a434f0631491
NDragneelL9 <nell.dragneel@gmail.com>
2022-01-08 09:54:53 +0300

    added spec for graphics page

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -1,11 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe 'Pages', type: :request do
+  let(:user) { FactoryGirl.create(:user) }
+  let(:personality) { FactoryGirl.create(:user_personality, user: user) }
+
+  before do
+    sign_in user
+  end
+
   describe 'GET /index' do
     it 'GET root page after authentication' do
-      user = FactoryGirl.create(:user)
-      sign_in(user)
       get root_path
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe 'GET /user_personalities/:id/graphics' do
+    it 'GET root page after authentication' do
+      get user_personality_graphics_path(personality)
       expect(response).to have_http_status(200)
     end
   end
